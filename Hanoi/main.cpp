ec258ed1bb2f18fd50557dbfe5b1474c04cca008
Sti11_Negat1ve <4rchitected@gmail.com>
2022-01-22 20:56:59 +0300

    Minories.

--- Hanoi/main.cpp
+++ Hanoi/main.cpp
@@ -21,10 +21,10 @@ void prnt() {
 }
 
 void hanoi(int from, int to, int start) {
-	int sub_high = twrs[from].size() - start; // Hight of the subtower to move
+	int sub_hight = twrs[from].size() - start; // Hight of the subtower to move
 
 	// Moving 1 disk;
-	if(sub_high == 1) {
+	if(sub_hight == 1) {
 		twrs[to].push_back(twrs[from].back());
 		twrs[from].pop_back();
 
@@ -33,11 +33,11 @@ void hanoi(int from, int to, int start) {
 	}
 
 	// Moving a subtower
-	int swp = 3 - (from + to); 		// Index of swap-tower
+	int swp = 3 - (from + to); 	// Index of swap-tower
 	int size_swp = twrs[swp].size();
-	hanoi(from, swp, start + 1);		// Move everything but first disk to swap.
-	hanoi(from, to, start);				// Move first disk to destination.
-	hanoi(swp, to, size_swp);			// Move swap to reconstruct subtower in the dest.
+	hanoi(from, swp, start + 1);	// Move everything but first disk to swap.
+	hanoi(from, to, start);			// Move first disk to destination.
+	hanoi(swp, to, size_swp);		// Move swap to reconstruct subtower in the dest.
 }
 
 int main(int argc, char** argv) {
