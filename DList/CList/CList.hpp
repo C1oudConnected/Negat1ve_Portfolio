15873af10d19515c746b824f9c1aeefb6bc0125b
Sti11_Negat1ve <4rchitected@gmail.com>
2022-01-26 14:25:27 +0300

    QTree and DList started.

--- DList/CList/CList.hpp
+++ DList/CList/CList.hpp
@@ -1,24 +1,23 @@
 #pragma once
-#include
+#include <memory>
 
 template<typename T>
-using TNodePtr = std::unique_ptr< CNode<T> >;
+using TNodePtr = std::unique_ptr< CListNode<T> >;
 
 template<typename T>
 class CList {
-
-};
-
-template<typename T>
-class CNode {
 public:
+
 private:
-	T val;
-	TNodePtr<T> pLeft;
-	TNodePtr<T> pRight;
-};
+	
+}
 
 template<typename T>
-class CListIterator : std::iterator {
-	
+class CListNode {
+public:
+
+private:
+	TNodePtr pPrev;
+	TNodePtr pNext;
+	T value;
 }
